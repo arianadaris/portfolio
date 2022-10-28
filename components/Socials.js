47ab389473834fcfd23f7b4dd4155a7139deaa4d
Ariana <73635827+arianadaris@users.noreply.github.com>
2022-10-28 12:33:50 -0700

    Updated Job section

--- components/Socials.js
+++ components/Socials.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
 import { Icon } from '@iconify/react';
 
 const Socials = () => {
   return (
     <div className="flex items-center text-24 space-x-12">
-        <Link href="/"><Icon className="hover:scale-125 transition" icon="akar-icons:dribbble-fill" /></Link>
-        <Link href="/"><Icon className="hover:scale-125 transition" icon="akar-icons:linkedin-box-fill" /></Link>
-        <Link href="/"><Icon className="hover:scale-125 transition" icon="akar-icons:github-fill" /></Link>
+        <a href="https://dribbble.com/arianadaris" target="_blank" rel="noreferrer"><Icon className="hover:scale-125 transition" icon="akar-icons:dribbble-fill" /></a>
+        <a href="https://www.linkedin.com/in/ariana-rajewski/" target="_blank" rel="noreferrer"><Icon className="hover:scale-125 transition" icon="akar-icons:linkedin-box-fill" /></a>
+        <a href="https://github.com/arianadaris" target="_blank" rel="noreferrer"><Icon className="hover:scale-125 transition" icon="akar-icons:github-fill" /></a>
     </div>
   )
 }
