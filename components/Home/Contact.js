79ed698f2a6cd23268c692079bb87e6b566a153c
Ariana Daris <arianarajewski@gmail.com>
2022-11-02 14:46:49 -0700

    Edit mobile animation

--- components/Home/Contact.js
+++ components/Home/Contact.js
@@ -7,13 +7,17 @@ import EmailButton from '../EmailButton';
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
+import useWindowSize from '../../hooks/useWindowSize';
+
 const Contact = () => {
     const symbol = "0x1F44B"; // waving hand emoji
     const control = useAnimation();
     const [ ref, inView ] = useInView();
+    const size = useWindowSize();
 
     useEffect(() => {
-        inView ? control.start('visible') : control.start('hidden');
+        if(size.width > 768)
+            inView ? control.start('visible') : control.start('hidden');
     }, [control, inView]);
 
     const scrollUp = {
@@ -22,10 +26,10 @@ const Contact = () => {
     };
 
     return (
-        <motion.div ref={ref} variants={scrollUp} initial="hidden" animate={control}>
+        <motion.div ref={ref} variants={scrollUp} initial={size.width > 768 ? "hidden" : "visible"} animate={control}>
             <StarredPhrase firstWord="Contact" secondWord="Me" />
             <h1 className="md:text-48">Let&apos;s Get In Touch!</h1>
-            <p className="xs:text-[5vw] md:text-24">Whether you&apos;re just saying <span role="img" aria-label="Wave">{String.fromCodePoint(symbol)}</span>, or you’re looking to start a new project, feel free to send me a message! Currently, I am open to remote freelancing work.</p>
+            <p className="xs:text-[5vw] md:text-24">Whether you&apos;re just saying <span role="img" aria-label="Wave">{String.fromCodePoint(symbol)}</span>, or you&apos;re looking to start a new project, feel free to send me a message! Currently, I am open to remote freelancing work.</p>
             <EmailButton />
         </motion.div>
     )
