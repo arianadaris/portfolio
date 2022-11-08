7233a9fcb23f764662185198e8a51b02dc5b9c75
Ariana <73635827+arianadaris@users.noreply.github.com>
2022-11-08 12:53:02 -0700

    Update spacing

--- components/OutlinedButton.js
+++ components/OutlinedButton.js
@@ -6,13 +6,13 @@ import { Icon } from '@iconify/react';
 import { useInView } from 'react-intersection-observer';
 
 const OutlinedButton = (props) => {
-    const { title, link } = props;
+    const { title, link, color} = props;
 
     return (
         <>
-            <Link className='flex items-center border-0.5 md:border-black xs:border-gray dark:border-white rounded-100 px-8 py-2 w-fit h-fit' href={ link }>
+            <Link className='flex items-center border-0.5 md:border-black xs:border-gray dark:border-white rounded-100 px-8 md:py-4 xs:py-1 w-fit h-fit' href={ link }>
                 <div className="flex items-center space-x-10 transition hover:scale-95 hover:cursor-pointer hover:animate-stop">
-                    <p className="">{ title }</p>
+                    <p className="dark:text-white">{ title }</p>
                     <Icon className="animate-slide xs:text-gray md:text-black dark:text-white" width={20} icon="bytesize:arrow-right" />
                 </div>
             </Link>
